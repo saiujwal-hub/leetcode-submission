dcbad7d6237760176d516550babc2561ab4218ab
saiujwal-hub <chinnumsaiujwal@gmail.com>
2026-06-15 17:30:42 +0530

    Time: 0 ms (100%), Space: 12 MB (6.04%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        vector<int> t=nums;
-        int n=nums.size();
+        vector<int> t = nums;
+        int n = nums.size();
+        
         for (int i = 0; i < nums.size(); i++) {
             t[i] = nums[i];
         }
-        sort(t.begin(), t.end());
-for(int x=0;x<=n;x++)
-{int c=0;
-    for(int i=0;i<n;i++)
-    {
-if(t[(i+x) % n]==nums[i])
-{
-    c++;
-}
-    }
-    if(c==n)
-    {
-        return true;
-    }
-}return false;
-    }};
-
         
+        sort(t.begin(), t.end());
+        
+        for (int x = 0; x <= n; x++) {
+            int c = 0;
+            for (int i = 0; i < n; i++) {
+                if (t[(i + x) % n] == nums[i]) {
+                    c++;
+                }
+            }
+            if (c == n) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
