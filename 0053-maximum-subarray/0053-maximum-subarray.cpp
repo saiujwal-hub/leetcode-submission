fb4a8bcad58b687216f19d8897b5803f986cb810
saiujwal-hub <chinnumsaiujwal@gmail.com>
2026-06-22 18:05:09 +0530

    Time: 0 ms (100%), Space: 71.8 MB (14.41%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       
+        int s=0;int m=INT_MIN;
+        for(int i=0;i<nums.size();i++)
+        {s+=nums[i];
+        if(s>m)
+        {m=s;
+
+        }
+         if(s<0)
+        {s=0;
+
+        }
+
+
+        }return m;
+
+        
+    }
+};
