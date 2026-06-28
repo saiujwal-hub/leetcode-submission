37c544cad0ccdf5e40b736ea553e0f755e9f27db
saiujwal-hub <chinnumsaiujwal@gmail.com>
2026-06-28 23:02:54 +0530

    Time: 0 ms (100%), Space: 23.5 MB (82.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int c1 = 0;int c2=0;
-        int el1;int el2;
+        int c1 = 0;
+        int c2 = 0;
+        int el1;
+        int el2;
         vector<int> v;
         for (int i = 0; i < nums.size(); i++) {
-            if (c1 == 0 && el2!=nums[i]) {
+            if (c1 == 0 && el2 != nums[i]) {
                 c1 = 1;
                 el1 = nums[i];
 
-            }
-           else  if (c2 == 0 && el1!=nums[i]) {
+            } else if (c2 == 0 && el1 != nums[i]) {
                 c2 = 1;
                 el2 = nums[i];
 
-            }
-              else if (el1 == nums[i]) {
+            } else if (el1 == nums[i]) {
                 c1++;
 
-            }
-            else if (el2 == nums[i]) {
+            } else if (el2 == nums[i]) {
                 c2++;
 
             } else {
-                c1--; c2--;
+                c1--;
+                c2--;
             }
-            
         }
-     
+
         int k = 0;
-        int d=0;
+        int d = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == el1) {
                 k++;
             }
             if (nums[i] == el2) {
                 d++;
-            }}
-            if (k > nums.size() / 3 ) {
-                v.push_back(el1);
             }
-            if (d > nums.size() / 3 ) {
-                v.push_back(el2);
-            }return v;
-    }};
-         
+        }
+        if (k > nums.size() / 3) {
+            v.push_back(el1);
+        }
+        if (d > nums.size() / 3) {
+            v.push_back(el2);
+        }
+        return v;
+    }
+};
